3473f0afaf7d49b3146e5580db45f3beb1362d6d
xvag <xvag@protonmail.com>
2022-05-24 17:05:56 +0300

    try nested loop

--- main.tf
+++ main.tf
@@ -42,7 +42,26 @@ resource "google_compute_firewall" "fw" {
   ]
 }
 
-module "create_vm_module" {
+resource "google_compute_instance" "vm" {
   for_each = var.vpc
-  source   = "./create_vm_module"
+  count = length(each.value.ip)
+  name         = "${each.value.name}-vm-${count.index}"
+  machine_type = each.value.machine
+  zone         = each.value.zone
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = each.value.image
+      size  = each.value.size
+    }
+  }
+
+  network_interface {
+    network    = "${each.value.name}-vpc"
+    subnetwork = "${each.value.name}-subnet"
+    network_ip = each.value.ip[count.index]
+    access_config {
+    }
+  }
 }
